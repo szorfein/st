e3b864a107f318db8f6ec84e39182b26a3b727a1
szorfein <szorfein@protonmail.com>
2021-06-19 07:47:07 +0200

    patch font2

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Terminal st customized (suckless)
 
 ## Patches
 + live-reload from [Xst](https://github.com/gnotclub/xst)
++ [font2](https://st.suckless.org/patches/font2/)
 
 ## Commands
 Use the `live-reload`:
--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Term :pixelsize=15:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = { "Iosevka Term :pixelsize=15:antialias=true:autohint=true" };
+
 static int borderpx = 2;
 
 /*
